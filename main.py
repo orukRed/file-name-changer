10e43d0dd3592cb41dc08c35dcf0d10dbbcd2494
orukRed <orukRed@gmail.com>
2024-10-18 00:32:41 +0900

    D&D対応

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, simpledialog, messagebox
+from tkinterdnd2 import TkinterDnD, DND_FILES
 
 
 def rename_files_in_folder(folder_path, target_string, replacement_string):
@@ -16,19 +17,29 @@ def rename_files_in_folder(folder_path, target_string, replacement_string):
 def select_folder():
     folder_path = filedialog.askdirectory()
     if folder_path:
-        target_string = simpledialog.askstring("Input", "置換対象の文字列を入力してください:")
-        replacement_string = simpledialog.askstring(
-            "Input", "置換後の文字列を入力してください:")
-        if target_string and replacement_string:
-            rename_files_in_folder(
-                folder_path, target_string, replacement_string)
-            messagebox.showinfo("完了", "ファイル名の置換が完了しました。")
-        else:
-            messagebox.showwarning("警告", "文字列が入力されていません。")
+        process_folder(folder_path)
+
+
+def process_folder(folder_path):
+    target_string = simpledialog.askstring("Input", "置換対象の文字列を入力してください:")
+    replacement_string = simpledialog.askstring("Input", "置換後の文字列を入力してください:")
+    if target_string and replacement_string:
+        rename_files_in_folder(folder_path, target_string, replacement_string)
+        messagebox.showinfo("完了", "ファイル名の置換が完了しました。")
+    else:
+        messagebox.showwarning("警告", "文字列が入力されていません。")
+
+
+def drop(event):
+    folder_path = event.data
+    if os.path.isdir(folder_path):
+        process_folder(folder_path)
+    else:
+        messagebox.showwarning("警告", "フォルダをドロップしてください。")
 
 
 # GUIの設定
-root = tk.Tk()
+root = TkinterDnD.Tk()
 root.title("ファイル名置換ツール")
 
 frame = tk.Frame(root, width=400, height=200)
@@ -40,5 +51,9 @@ label.pack(pady=20)
 
 button = tk.Button(frame, text="フォルダを選択", command=select_folder)
 button.pack(pady=20)
+
+# ドラッグアンドドロップの設定
+root.drop_target_register(DND_FILES)
+root.dnd_bind('<<Drop>>', drop)
 
 root.mainloop()
